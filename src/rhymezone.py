906d66fbff060282ef37aa6792ec89588ef35c67
manhdt <thitkhotau.no1@gmail.com>
2024-05-19 17:46:38 +0700

    ...

--- src/rhymezone.py
+++ src/rhymezone.py
@@ -1,15 +1,17 @@
 from dataclasses import dataclass
-from requests import get
+from typing import Optional
+
 from bs4 import BeautifulSoup
+from requests import get
 
 
 @dataclass
 class RhymeZone:
     word: str
 
-    def words(self):
+    def words(self, timeout: Optional[int] = None):
         url = f"https://www.rhymezone.com/r/rhyme.cgi?Word={self.word}&typeofrhyme=cons&org1=syl@org2=l&org3=y"
-        response = get(url, headers={"User-Agent": "Mozilla/5.0"})
+        response = get(url, headers={"User-Agent": "Mozilla/5.0"}, timeout=timeout)
         soup = BeautifulSoup(response.text, "html.parser")
         for r in soup.find_all("a", class_="r"):
             yield r.text
