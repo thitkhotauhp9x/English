5031a7795e4abb86565c5e363781fc3a4aa065ef
manhdt <thitkhotau.no1@gmail.com>
2024-05-19 18:33:10 +0700

    ...

--- src/oxford_learners_dictionaries.py
+++ src/oxford_learners_dictionaries.py
@@ -1,25 +1,26 @@
 from dataclasses import dataclass
-from requests import get
+from typing import Optional
+
 from bs4 import BeautifulSoup
+from requests import get
 
 
 @dataclass
 class OxfordLearnerDictionaries:
     word: str
 
-    @property
-    def phonetic_br(self):
+    def phonetic_br(self, timeout: Optional[int] = None):
         url = f"https://www.oxfordlearnersdictionaries.com/definition/english/{self.word}"
-        response = get(url, headers={"User-Agent": "Mozilla/5.0"})
+        response = get(url, headers={"User-Agent": "Mozilla/5.0"}, timeout=timeout)
         soup = BeautifulSoup(response.text, "html.parser")
 
         container = soup.find("div", class_="top-container")
         if container is None:
             return None
-        phones_br = container.find("div", class_="phons_br")
+        phones_br = container.find("div", class_="phons_br")  # type: ignore
         if phones_br is None:
             return None
-        phon = phones_br.find("span", class_="phon")
+        phon = phones_br.find("span", class_="phon")  # type: ignore
         if phon is None:
             return None
-        return phon.text
+        return phon.text  # type: ignore
