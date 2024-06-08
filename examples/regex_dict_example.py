5aa74de05da7be24acbb9913688bdbb488ed85c4
manhdt <thitkhotau.no1@gmail.com>
2024-06-08 14:38:36 +0700

    ...

--- examples/regex_dict_example.py
+++ examples/regex_dict_example.py
@@ -1,9 +1,25 @@
-from english.utilities import RegexDict
+import pickle
+from pathlib import Path
+
+from tqdm import tqdm
+
+from english.utilities import RegexDict, OxfordLearnerDictionaries
 
 
 def main():
-    for value in RegexDict("ear").find():
-        print(value)
+    path = Path("data.pickle")
+    if not path.exists():
+        data = []
+        for word in tqdm(list(RegexDict("ear").find())):
+            phonetic = OxfordLearnerDictionaries(word).phonetic_br()
+            data.append((word, phonetic))
+
+        with open(path.as_posix(), "wb") as f:
+            pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
+
+    with open(path.as_posix(), "rb") as f:
+        data = pickle.load(f)
+        print(data)
 
 
 if __name__ == "__main__":
