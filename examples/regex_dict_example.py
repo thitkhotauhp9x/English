5292f9c5f09fdde16886a9954986f9794cec2d45
manhdt <thitkhotau.no1@gmail.com>
2024-06-09 01:12:47 +0700

    ...

--- examples/regex_dict_example.py
+++ examples/regex_dict_example.py
@@ -1,31 +1,37 @@
 import pickle
 from pathlib import Path
+from typing import Generator, Tuple
 
 from tqdm import tqdm
 
 from english.utilities import RegexDict, OxfordLearnerDictionaries, count_syllables
 
 
-def main():
+def filter_(word: str, num: int) -> Generator[Tuple[str, str], None, None]:
     path = Path("data.pickle")
     if not path.exists():
         data = []
-        for word in tqdm(list(RegexDict("ear").find())):
-            phonetic = OxfordLearnerDictionaries(word).phonetic_br()
-            data.append((word, phonetic))
+        for word_ in tqdm(list(RegexDict(word).find())):
+            phonetic = OxfordLearnerDictionaries(word_).phonetic_br()
+            data.append((word_, phonetic))
 
         with open(path.as_posix(), "wb") as f:
             pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
 
     with open(path.as_posix(), "rb") as f:
-        for word, phonetic in pickle.load(f):
+        for word_, phonetic in pickle.load(f):
             if phonetic is None:
                 continue
             total = count_syllables(phonetic)
-            if total == 1:
-                print(word, phonetic)
+            if total == num:
+                yield word_, phonetic
             else:
                 ...
+
+
+def main():
+    for word, phonetic in filter_("ear", 1):
+        print(word, phonetic)
 
 
 if __name__ == "__main__":
