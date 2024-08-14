279c9ce4d2aa25a79a6f356e4b6e847056c23d28
manhdt <thitkhotau.no1@gmail.com>
2024-08-14 13:06:49 +0700

    Adding ruff for pre-commit

--- english/utilities/__init__.py
+++ english/utilities/__init__.py
@@ -3,5 +3,14 @@ from english.utilities._oxford_learners_dictionaries import OxfordLearnerDiction
 from english.utilities._phonetic import Phonetic
 from english.utilities._regex_dict import RegexDict
 from english.utilities._rhymezone import RhymeZone
+from english.utilities._phonetic_tag import PhoneticTag
 
-__all__ = ["RegexDict", "RhymeZone", "OxfordLearnerDictionaries", "count_syllables", "find_vowel", "Phonetic"]
+__all__ = [
+    "RegexDict",
+    "RhymeZone",
+    "OxfordLearnerDictionaries",
+    "count_syllables",
+    "find_vowel",
+    "Phonetic",
+    "PhoneticTag",
+]
