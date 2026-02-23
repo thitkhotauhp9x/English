fd72967b62bc9bcd5370f43d8273ca8ad9d067cd
manhdt <thitkhotau.no1@gmail.com>
2026-02-23 12:49:26 +0700

    Update fontend

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState, type SetStateAction} from "react";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState('');
+
+  function handleClick() {
+    console.log(text);
+  }
+
+  function handleChange(e: { target: { value: SetStateAction<string>; }; }) {
+    setText(e.target.value);
+  }
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <label>Search</label>
+        <input value={text} onChange={handleChange}></input>
+        <br/>
+        <button onClick={handleClick}>Submit</button>
+        <br/>
+        <div>
+          Result
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
